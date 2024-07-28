f35158dd69e5e7dee87f40592e956ea58a7c1de7
Elykplime <akakpoyvettee@gmail.com>
2024-07-28 09:55:25 +0000

    file modified

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -56,6 +56,20 @@ document.addEventListener("DOMContentLoaded", () => {
     function filterQuotes() {
       showRandomQuote();
     }
+
+    function createAddQuoteForm() {
+        const formContainer = document.getElementById('quoteFormContainer');
+        
+        const formHtml = `
+          <div>
+            <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+            <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+            <button onclick="addQuote()">Add Quote</button>
+          </div>
+        `;
+      
+        formContainer.innerHTML = formHtml;
+      }
   
     addQuoteForm.addEventListener("submit", async (event) => {
       event.preventDefault();
